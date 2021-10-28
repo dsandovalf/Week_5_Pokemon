4e454c9e1be9cf6c0387c9aa4678bd006329a23c
dsandovalf <dasanflo@gmail.com>
2021-10-28 16:16:07 -0500

    day 3 commit

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,60 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import requests
 from app import app
-from .forms import SearchForm
+from .forms import LoginForm, RegisterForm, SearchForm
+from.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html.j2')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = request.form.get("email").lower()
+        password = request.form.get("password")
+        u = User.query.filter_by(email = email).first()
+        if u and u.check_hashed_password(password):
+            login_user(u)
+            return redirect(url_for("index"))
+        error_string = "Invalid Email password combo"
+        return render_template('login.html.j2', error = error_string, form=form)
+    return render_template('login.html.j2', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    if current_user:
+        logout_user()
+        return redirect(url_for("login"))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        try:
+            new_user_data = {
+                "first_name":form.first_name.data.title(),
+                "last_name":form.last_name.data.title(),
+                "email":form.email.data.lower(),
+                "password": form.password.data
+            }
+            new_user_object = User()
+            # build user with form data
+            new_user_object.from_dict(new_user_data)
+            # save user to database
+            new_user_object.save()
+        except:
+            error_string = "There was an unexpected Error creating your account. Please Try again."
+            return render_template('register.html.j2',form=form, error = error_string)
+        return redirect(url_for('login'))
+    return render_template('register.html.j2', form = form)
+
 @app.route('/pokemon', methods=['GET', 'POST'])
+@login_required
 def pokemon():
     form = SearchForm()
     if request.method == 'POST':
@@ -16,9 +63,6 @@ def pokemon():
         response = requests.get(url)
         print(url)
         if response.ok:
-            if not response.json().get("forms"):
-                error_string ="We had an error loading your data."
-                return render_template('pokemon.html.j2', error = error_string, form=form)
             data = response.json()
             all_pokemon=[]
             pokemon_dict={
